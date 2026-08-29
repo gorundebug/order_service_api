16cb28d83b0162404ecf5816dd79ca3184b57c3b
local <local@dev>
2026-08-29 07:35:46 +0300

    release v0.2.28

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Go module path: `github.com/gorundebug/order_service_api`
 ## Make commands
 
 ```bash
-make gen-proto           # generate protobuf code
-make fmt-proto           # format .proto files
-make gen-openapi         # generate OpenAPI client/server types (via oapi-codegen)
+make gen-proto           # [host] generate protobuf code
+make fmt-proto           # [host] format .proto files
+make gen-openapi         # [host] generate OpenAPI client/server types (via oapi-codegen)
 ```
--- make.generated.mk
+++ make.generated.mk
@@ -2,15 +2,21 @@
 MODULE_DIR := $(abspath .)
 TOOLS_DIR ?= $(abspath ../tools)
 BUF := $(TOOLS_DIR)/buf
-PROTOC ?= $(TOOLS_DIR)/protoc
+PROTOC ?= $(or $(shell command -v protoc 2>/dev/null),$(TOOLS_DIR)/protoc)
 DEPENDENCY_DOCKER_TARGETS :=
 include dependency-proxy.generated.mk
 OAPI_CODEGEN_VERSION := v2.4.1
 OAPI_CODEGEN := $(TOOLS_DIR)/oapi-codegen
 
-.PHONY: all gen-proto fmt-proto gen-openapi
+.PHONY: all build test gen-proto fmt-proto gen-openapi
 
 all: gen-proto gen-openapi
+
+build: all ## [host] Compile every package in this module
+	@go test -run '^$$' ./...
+
+test: all ## [host] Run this module's tests
+	@go test ./...
 
 gen-proto:
 	@find . -type f -name 'go.generated.proto.mk' | while read mkfile; do \
